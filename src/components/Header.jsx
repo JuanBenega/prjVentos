4b1704b1b4e442fd3e01b8e294c3075f26c35c8c
juan benega <jmbenega@gmail.com>
2023-01-19 17:49:49 -0300

    add navbar animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,40 @@
 import { Navbar, Nav, Image, Button } from "react-bootstrap"
 import { useEffect, useState } from "react";
 import logo from '../img/logo.png'
+import logoDesarm from '../img/logoDesarm.png'
 import locationIcon from '../img/locationIcon.svg'
 import phoneIcon from '../img/phoneIcon.svg'
 
 const Header = () => {
 
-    const [scrollTop, setScrollTop] = useState(false);
+    //const [scrollY, setScrollY] = useState(false);
+    //const styleSup = "text Sup", styleInf = "text Inf";
+
+    const [offset, setOffset] = useState(0);
 
     useEffect(() => {
-        const handleScroll = event => {
-            let scroll = window.scrollY;
-            scroll >= 130 ? console.log("MAYOR"): console.log("MENOR");
-            console.log(scroll);
-        };
-    
-        window.addEventListener('scroll', handleScroll);
-    
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
+        const onScroll = () => setOffset(window.pageYOffset);
+        // clean up code
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
 
     return (
         <Navbar collapseOnSelect expand="lg" className="navClass" variant="dark" fixed="top">
-            <div className="text Sup">
-                <Image src={locationIcon} />
-                <p>Perdriel 2875, La Tablada, Buenos Aires</p>
-                <Image src={phoneIcon} />
-                <p>(011) 15-41743095</p>
-            </div>
-            <Nav className="header">
-                <Image src={logo} className="logoHeader"/>
+            {offset < 130
+                ? <div className="text Sup">
+                    <Image src={locationIcon} />
+                    <p>Perdriel 2875, La Tablada, Buenos Aires</p>
+                    <Image src={phoneIcon} />
+                    <p>(011) 15-41743095</p>
+                </div>
+                : <div className="blackDiv"></div>
+            }
+            <Nav className={offset < 130 ? "header" : "header thin"}>
+                <Image src={offset < 130 ? logo : logoDesarm} className={offset < 130 ? "logoHeader" : "logoHeader simple"} />
+                {/* <div className="logoHeader"></div> */}
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="linkGroup">
                     <Nav >
@@ -43,9 +46,12 @@ const Header = () => {
                     <Button className="bContact">Contacto</Button>
                 </Navbar.Collapse>
             </Nav>
-            <Nav className="text Inf">
-                <p>DEVELOPER INDUSTRIAL AUTOMATION</p>
-            </Nav>
+            {offset < 130
+                ? <div className="text Inf">
+                    <p>DEVELOPER INDUSTRIAL AUTOMATION</p>
+                </div>
+                : <div className="blackDiv"></div>
+            }
 
         </Navbar>
     )
