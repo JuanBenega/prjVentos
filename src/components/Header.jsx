ab2eb97519b599dc985d0eeffae6dc3f1b460545
juan benega <jmbenega@gmail.com>
2023-01-19 16:29:06 -0300

    adecuacion logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,29 @@
-import { Container, Navbar, Nav, Image, Button } from "react-bootstrap"
+import { Navbar, Nav, Image, Button } from "react-bootstrap"
+import { useEffect, useState } from "react";
 import logo from '../img/logo.png'
 import locationIcon from '../img/locationIcon.svg'
 import phoneIcon from '../img/phoneIcon.svg'
 
 const Header = () => {
+
+    const [scrollTop, setScrollTop] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = event => {
+            let scroll = window.scrollY;
+            scroll >= 130 ? console.log("MAYOR"): console.log("MENOR");
+            console.log(scroll);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
     return (
-        <Navbar collapseOnSelect expand="lg" className="navClass" variant="dark" sticky="top">
+        <Navbar collapseOnSelect expand="lg" className="navClass" variant="dark" fixed="top">
             <div className="text Sup">
                 <Image src={locationIcon} />
                 <p>Perdriel 2875, La Tablada, Buenos Aires</p>
@@ -13,7 +31,7 @@ const Header = () => {
                 <p>(011) 15-41743095</p>
             </div>
             <Nav className="header">
-                <Image src={logo} />
+                <Image src={logo} className="logoHeader"/>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="linkGroup">
                     <Nav >
