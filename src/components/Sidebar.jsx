4f502868d75edd9cd3b890fd0c6a6b05b6ebbaf5
juan benega <jmbenega@gmail.com>
2023-01-24 19:13:06 -0300

    sidebar button, content, style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Offcanvas, Image, NavLink, OffcanvasTitle } from 'react-bootstrap';
+import sidebar from '../img/sidebar.png'
+
 
 const Sidebar = () => {
 
@@ -11,16 +12,29 @@ const Sidebar = () => {
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow} className="me-2">
-                Contact
-            </Button>
-            <Offcanvas show={show} onHide={handleClose} placement='end'>
+            <Image src={sidebar} onClick={handleShow} className="sidebarBtn" />
+            <Offcanvas show={show} onHide={handleClose} placement='end' className='sidebarCont'>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                    <OffcanvasTitle></OffcanvasTitle>
                 </Offcanvas.Header>
-                <Offcanvas.Body>
-                    Some text as placeholder. In real life you can have the elements you
-                    have chosen. Like, text, images, lists, etc.
+                <Offcanvas.Body className='sidebarText'>
+                    <h5>Telefóno</h5>
+                    <h6>Buenos aires: +54-911-0000-0000</h6>
+                    <h6>Mendoza: +54-9261-0000-0000</h6>
+                    <h6>San Luis: +54-9000-0000-0000</h6>
+                    <br />
+                    <br />
+                    <h5>Redes sociales</h5>
+                    <NavLink className='sidebarLink' target="_blank" href='https://www.facebook.com/profile.php?id=100083334182810'>Facebook: Ventos</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='https://www.instagram.com/ventos_oficial/'>Instagram: Ventos_oficial</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href=''>Youtube: @Ventos_oficial</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='https://www.linkedin.com/company/ventoselect-s-r-l/'>Linkedin: Ventoselect-s-r-l</NavLink>
+                    <br />
+                    <br />
+                    <h5>Correo electrónico</h5>
+                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>info@vnetos.com.ar</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>mail2@vnetos.com.ar</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>mail3@vnetos.com.ar</NavLink>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
