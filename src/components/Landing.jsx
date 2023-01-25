1fbeb60d47dea505f33ab490229eac8bb6ffa29a
juan benega <jmbenega@gmail.com>
2023-01-24 21:44:13 -0300

    add & style carousel

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,27 +8,36 @@ import landing3 from '../img/landing3.jpg'
 const Landing = () => {
     return (
         <div className="landing">
-            <Carousel className='landCarousel'>
-                <Carousel.Item>
+            <Carousel fade className='landCarousel'>
+                <Carousel.Item className='carouItem'>
                     <img
                         className="d-block w-100"
                         src={landing1}
                         alt="First slide"
                     />
+                    <Carousel.Caption className='carouText'>
+                        <h1>Somos una empresa apasionada por lo que hacemos</h1>
+                    </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className='carouItem'>
                     <img
                         className="d-block w-100"
                         src={landing2}
                         alt="Second slide"
                     />
+                    <Carousel.Caption className='carouText'>
+                        <h1>La innovación es nuestra premisa</h1>
+                    </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className='carouItem'>
                     <img
                         className="d-block w-100"
                         src={landing3}
                         alt="Third slide"
                     />
+                    <Carousel.Caption className='carouText'>
+                        <h1>Crecemos cuando crecen nuestros clientes</h1>
+                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
