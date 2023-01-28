f8b59cc02ac5e62a3473b0d78dc3569d58a54003
juan benega <jmbenega@gmail.com>
2023-01-28 19:48:34 -0300

    add tooltip in locationPin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,9 @@
 import { Image } from 'react-bootstrap'
 import mapa from '../img/mapa.png'
 import locationPin from '../img/locationPin.png'
+import contactPhone from '../img/contactPhone.png'
+import contactGps from '../img/contactGps.png'
+import contactMail from '../img/contactMail.png'
 
 const Contact = () => {
     return (
@@ -9,8 +12,19 @@ const Contact = () => {
                 <h1 className="contactTitle">Nos podés encontrar a través del país</h1>
                 <h4 className="contactText">Somos una empresa en continua expansión</h4>
                 <Image src={mapa} className='contactMap' />
-                <Image src={locationPin} className='location1' />
-                <Image src={locationPin} className='location2' />
+                <div className="location1 tooltip">
+                    <Image src={locationPin} />
+                    <div className="right">
+                        <Image src={contactGps} />
+                        <h5>Perdriel 2875, La Tablada, Buenos Aires</h5>
+                        <Image src={contactPhone} />
+                        <h5>(011) 15-41743095</h5>
+                        <Image src={contactMail} />
+                        <h5>abc@ventos.com.ar</h5>
+                        <i></i>
+                    </div>
+                </div>
+                {/* <Image src={locationPin} className='location2' /> */}
             </div>
         </div>
     )
