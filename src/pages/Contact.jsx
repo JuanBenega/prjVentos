82ed37eabf28f0aa9895cc4ee5c40ff4c8bc2c01
juan benega <jmbenega@gmail.com>
2023-01-30 16:53:26 -0300

    add social, phone & location

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,14 +21,14 @@ const Contact = () => {
             <div className="map">
                 <h1 className="contactTitle">Nos podés encontrar a través del país</h1>
                 <h4 className="contactText">Somos una empresa en continua expansión</h4>
-                <Image src={mapa} className='contactMap' />
+                {/* <Image src={mapa} className='contactMap' /> */}
                 <div className="location1 tooltip">
                     <Image src={locationPin} />
                     <div className="right">
                         <Image src={contactGps} />
                         <h6>Perdriel 2875, La Tablada, Buenos Aires</h6>
                         <Image src={contactPhone} />
-                        <h6>(011) 15-41743095</h6>
+                        <h6>(011) 15-4174-3095</h6>
                         <Image src={contactMail} />
                         <h6>info@ventos.com.ar</h6>
                         <i></i>
@@ -48,8 +48,20 @@ const Contact = () => {
                 </div>
             </div>
             <div className="contactLinks">
-                <div className="contactSocial">
-                    <Nav.Link href=''><Image src={contactFace} /></Nav.Link>
+                <div className="contactItem">
+                    <h6>Seguinos</h6>
+                    <Nav.Link href='https://www.facebook.com/profile.php?id=100083334182810'><Image src={contactFace} /></Nav.Link>
+                    <Nav.Link href='https://www.instagram.com/ventos_oficial/'><Image src={contactInst} /></Nav.Link>
+                    <Nav.Link href=''><Image src={contactTwit} /></Nav.Link>
+                    <Nav.Link href='https://www.linkedin.com/company/ventoselect-s-r-l/'><Image src={contactLinkedin} /></Nav.Link>
+                </div>
+                <div className="contactItem">
+                    <Image src={contactLinkPhone} />
+                    <h6>(011) 15-4174-3095</h6>
+                </div>
+                <div className="contactItem2">
+                <Image src={contactLinkGps} />
+                    <h6>Perdriel 2875, La Tablada, Buenos Aires</h6>
                 </div>
             </div>
         </div>
