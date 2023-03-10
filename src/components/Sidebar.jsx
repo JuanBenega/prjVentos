109cb1857fa363c91cf64e2822cfba18a9c7f354
juan benega <jmbenega@gmail.com>
2023-03-10 11:59:38 -0300

    modif sidebar info

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,22 +19,20 @@ const Sidebar = () => {
                 </Offcanvas.Header>
                 <Offcanvas.Body className='sidebarText'>
                     <h5>Telefóno</h5>
-                    <h6>Buenos aires: +54-911-4174-3095</h6>
-                    <h6>Mendoza: +54-9261-0000-0000</h6>
-                    <h6>San Luis: +54-9000-0000-0000</h6>
-                    <br />
+                    <h6>Buenos aires: +54-911-6983-2386 (JORGE)</h6>
+                    <h6>Mendoza: +54-92612-08-1375 (NICOLAS)</h6>
+                    <h6>San Luis: +54-911-3112-9539 (FRANCO)</h6>
                     <br />
                     <h5>Redes sociales</h5>
                     <NavLink className='sidebarLink' target="_blank" href='https://www.facebook.com/profile.php?id=100083334182810'>Facebook: Ventos</NavLink>
                     <NavLink className='sidebarLink' target="_blank" href='https://www.instagram.com/ventos_oficial/'>Instagram: Ventos_oficial</NavLink>
-                    <NavLink className='sidebarLink' target="_blank" href=''>Youtube: @Ventos_oficial</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='https://www.youtube.com/@ventos_oficial'>Youtube: @Ventos_oficial</NavLink>
                     <NavLink className='sidebarLink' target="_blank" href='https://www.linkedin.com/company/ventoselect-s-r-l/'>Linkedin: Ventoselect-s-r-l</NavLink>
-                    <br />
                     <br />
                     <h5>Correo electrónico</h5>
                     <NavLink className='sidebarLink' target="_blank" href='mailto:'>info@vnetos.com.ar</NavLink>
-                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>mail2@vnetos.com.ar</NavLink>
-                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>mail3@vnetos.com.ar</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>ventas@ventos.com.ar</NavLink>
+                    <NavLink className='sidebarLink' target="_blank" href='mailto:'>administracion@ventos.com.ar</NavLink>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
